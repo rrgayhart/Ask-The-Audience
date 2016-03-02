d48943a1392a64cff6379d527573174977c2da77
rrgayhart <rrgayhart@gmail.com>
2016-03-02 10:14:58 -0700

    Show connected users

--- server.js
+++ server.js
@@ -11,9 +11,22 @@ app.get('/', function(req, res){
 
 var port = process.env.PORT || 3000;
 
-var server = http.createServer(app);
+var server = http.createServer(app)
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
 server.listen(port, function(){
   console.log('Listening on port ' + port + '.');
 });
 
+io.on('connection', function(socket){
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.on('disconnect', function(){
+    console.log('A user has disconnected.', io.engine.clientsCount);
+  });
+});
 module.exports = server;
