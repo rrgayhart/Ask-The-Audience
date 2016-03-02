0caba025a0071d4b220a0f34c73033518148c0ca
rrgayhart <rrgayhart@gmail.com>
2016-03-02 10:47:38 -0700

    Complete base tutorial functionality

--- public/client.js
+++ public/client.js
@@ -5,3 +5,21 @@ var connectionCount = document.getElementById('connection-count');
 socket.on('usersConnected', function(count){
   connectionCount.innerText = 'Connected Users: ' + count;
 });
+
+var statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
+
+socket.on('voteCount', function (votes){
+  console.log(votes);
+});
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const express = require('express');
 
 const app = express();
 
+var votes = {};
+
+function countVotes(votes){
+  var voteCount = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0
+  };
+
+  for (var vote in votes) {
+    voteCount[votes[vote]]++
+  }
+  return voteCount;
+};
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
@@ -25,8 +41,21 @@ io.on('connection', function(socket){
 
   io.sockets.emit('usersConnected', io.engine.clientsCount);
 
+  socket.emit('statusMessage', 'You have connected.');
+
   socket.on('disconnect', function(){
-    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('userConnection', io.engine.clientsCount);
+  });
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast'){
+      votes[socket.id] = message;
+      socket.emit('voteCount', countVotes(votes));
+    }
   });
 });
+
+
 module.exports = server;
